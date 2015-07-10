f38d4091e5a3c1ed69a6e80277ed9b9b16c4ea0a
Ioannis Canellos <iocanel@gmail.com>
2015-07-10 17:40:43 +0300

    Fix .ssh path

--- start-jenkins-slave.sh
+++ start-jenkins-slave.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #copy ssh keys
-mkdir -p /root/jenkins/.ssh
-chmod 700 /root/jenkins/.ssh
+mkdir -p /root/.ssh
+chmod 700 /root/.ssh
 cp /home/jenkins/ssh-keys/authorized_keys /root/.ssh/authorized_keys
 chown -R root:root /root/.ssh
 chmod 600 /root/.ssh/authorized_keys
